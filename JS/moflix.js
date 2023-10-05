74554af1a61702c0235e740e70df84a7a55088de
gggwzj <gggicfpl@qq.com>
2023-10-05 17:37:38 +0000

    上传文件至 'JS'

--- JS/moflix.js
+++ JS/moflix.js
@@ -4,8 +4,8 @@ var rule = {
     模板:'短视2',
     host: 'http://4.567.li',
     homeUrl:'/map.html',
-    // url: '/index.php/api/vod#type=fyclass&page=fypage',
-    url: '/index.php/api/vod#type=fyfilter&page=fypage',
+    url: '/index.php/api/vod#type=fyclass&page=fypage',
+    //url: '/index.php/api/vod#type=fyfilter&page=fypage',
     filterable:1,//是否启用分类筛选,
     filter_url:'{{fl.cateId}}',
     filter: {
@@ -23,7 +23,7 @@ var rule = {
     class_name:'',
     class_url:'',
     detailUrl:'/index.php/vod/detail/id/fyid.html',
-    推荐:'.border-box .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
+    //推荐:'.border-box .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
     double: false, // 推荐内容是否双层定位
     一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=it.vod_pic.replace("mac:","https:");return it});',
 }
